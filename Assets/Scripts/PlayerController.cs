020d14ae38f71f2f8bf29a410d15c56e976d9b90
Ardaa891 <ardaaydar@funikagames.com>
2022-03-16 15:42:42 +0300

    16.03.2022

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,13 +22,18 @@ public class PlayerController : MonoBehaviour
     public float yOffset ;
     Sequence seq;
     public int score = 0;
+    public float currentZ;
+    Color color1, color2;
+    public Material  gateMaterial, badGateMaterial;
 
     private void Awake()
     {
+       
         Currrent = this;
         follower = GetComponent<SplineFollower>();
         //offset = new Vector2(xOffset, 0);
         seq = DOTween.Sequence();
+        
         
     }
 
@@ -70,38 +75,52 @@ public class PlayerController : MonoBehaviour
                 offset = transform.GetComponent<SplineFollower>().motion.offset.x +  25f * touchXDelta * Time.fixedDeltaTime;
                 offset = Mathf.Clamp(offset, -7f, 7f);
                 transform.GetComponent<SplineFollower>().motion.offset = new Vector3(offset, yOffset);
-                
+
+                if (touchXDelta >= -7f && touchXDelta < 0)
+                {
+                    
+                    //transform.GetComponent<SplineFollower>().motion.rotationOffset += new Vector3(0, -rotY)*5*Time.fixedDeltaTime;
+                    transform.DORotate(new Vector3(0, -15, 0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(()=>Rot());
+
+
+
+
+                }
+                else if (touchXDelta <= 7f && touchXDelta > 0)
+                {
+                    
+                    transform.DORotate(new Vector3(0, 24, 0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot1());
+
+
+
+
+                }
+
             }
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
             {
                 
                 touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
                 offset = transform.GetComponent<SplineFollower>().motion.offset.x + 25f * touchXDelta * Time.fixedDeltaTime;
-                offset = Mathf.Clamp(offset, -9f, 9f);
+                offset = Mathf.Clamp(offset, -7f, 7f);
                 transform.GetComponent<SplineFollower>().motion.offset = new Vector3(offset, yOffset );
 
+                if (touchXDelta >= -7f && touchXDelta < 0)
+                {
+                    transform.DORotate(new Vector3(0, -15, 0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot1());
+
+
+
+
+                }
+                else if (touchXDelta <= 7f && touchXDelta > 0)
+                {
+                    transform.DORotate(new Vector3(0, 15, 0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot());
+                }
+
             }
 
-            /*if (score >= 10 )
-            {
-                
-                cube.SetActive(false);
-                house1.SetActive(true);
-               
-            }
-
-            if (score >= 20)
-            {
-                house2.SetActive(true);
-                house1.SetActive(false);
-                
-            }
-
-            if(score < 10)
-            {
-                house1.SetActive(false);
-                cube.SetActive(true);
-            }*/
+            
 
         }
         else
@@ -129,7 +148,14 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Gate"))
         {
             IncreaseScore(5);
-            other.gameObject.SetActive(false);
+            //other.gameObject.SetActive(false);
+            transform.DOScale(1.1f, 0.3f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+            //ColorUtility.TryParseHtmlString("D6D6D6", out color1);
+            //other.GetComponent<Image>().color = color1;
+            other.gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material = gateMaterial;
+
+
+
             if (score >= 10)
             {
 
@@ -187,12 +213,18 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("BadGate"))
         {
             DecreaseScore(5);
-            other.gameObject.SetActive(false);
+            //other.gameObject.SetActive(false);
+            transform.DOScale(1.03f, 0.3f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+            //ColorUtility.TryParseHtmlString("D6D6D6", out color1);
+            other.gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material = badGateMaterial;
+
             if (score >= 10)
             {
 
                 tudorHouse.SetActive(false);
                 dutchHouse.SetActive(true);
+                
+                
 
             }
 
@@ -207,6 +239,28 @@ public class PlayerController : MonoBehaviour
             {
                 dutchHouse.SetActive(false);
                 tudorHouse.SetActive(true);
+                /*seq.Join(tudorHouse.transform.DOLocalRotate(new Vector3(0, 890, 0), 0.7f).SetEase(Ease.OutQuad));
+                seq.Join(tudorHouse.transform.DOLocalMoveY(2.15f, 0.3f)).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad);
+                seq.Join(tudorHouse.transform.DOScale(1, 0.7f)).SetEase(Ease.InOutQuad);*/
+                
+            }
+
+            if (score > 10 && score < 20)
+            {
+                frenchHouse.SetActive(false);
+                dutchHouse.SetActive(true);
+            }
+
+            if (score > 20 && score < 30)
+            {
+                frenchHouse.SetActive(true);
+                spanishHouse.SetActive(false);
+            }
+
+            if (score > 30 && score < 40)
+            {
+                spanishHouse.SetActive(true);
+                englishHouse.SetActive(false);
             }
         }
 
@@ -220,6 +274,16 @@ public class PlayerController : MonoBehaviour
     public void DecreaseScore(int increment)
     {
         score -= increment;
+    }
+
+    void Rot()
+    {
+        transform.DOLocalRotate(new Vector3(0,9.26f,0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear);
+    }
+
+    void Rot1()
+    {
+        transform.DOLocalRotate(new Vector3(0,9.26f,0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear);
     }
 
 }
