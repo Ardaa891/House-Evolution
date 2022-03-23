ffc083904f2d98b06761705d3c0b54c49a279785
Ardaa891 <ardaaydar@funikagames.com>
2022-03-23 15:30:15 +0300

    23.03.2022

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     private float _currentSpeed;
-    public float xSpeed;
+    
     public float limitX;
     Rigidbody rb;
     public SplineFollower follower;
@@ -20,14 +20,15 @@ public class PlayerController : MonoBehaviour
     public GameObject tudorHouse, dutchHouse, frenchHouse, spanishHouse, englishHouse;
     public float offset;
     public float yOffset ;
-    public float rotOffset;
+    public float calendarOffset;
+    public Material skyboxMat;
     Sequence seq;
     public float score = 0;
     public int date;
     public Camera cam;
     public GameObject _cam;
     public GameObject calendar;
-    
+    public TextMeshProUGUI popup;
     public Material  gateMaterial, badGateMaterial;
 
     private void Awake()
@@ -78,23 +79,20 @@ public class PlayerController : MonoBehaviour
                 touchXDelta = Input.GetAxis("Mouse X");
                 offset = transform.GetComponent<SplineFollower>().motion.offset.x +  25f * touchXDelta * Time.fixedDeltaTime;
                 offset = Mathf.Clamp(offset, -6f, 7f);
+                calendarOffset = transform.GetComponent<SplineFollower>().motion.offset.x + 30f * touchXDelta * Time.fixedDeltaTime;
+                calendarOffset = Mathf.Clamp(calendarOffset, -6f, 7f);
                 transform.GetComponent<SplineFollower>().motion.offset = new Vector3(offset, yOffset);
-                calendar.transform.GetComponent<SplineFollower>().motion.offset = new Vector3(offset, yOffset);
-                /*rotOffset = transform.GetComponent<SplineFollower>().motion.rotationOffset.y + 100f* touchXDelta * Time.fixedDeltaTime;
-                rotOffset = Mathf.Clamp(rotOffset, -6f, 24f);
-                transform.GetComponent<SplineFollower>().motion.rotationOffset = new Vector3(0, rotOffset);*/
-
+                calendar.transform.GetComponent<SplineFollower>().motion.offset = new Vector3(calendarOffset, 0.7f);
+                
                
                 if (touchXDelta >= -6f && touchXDelta < 0)
                 {
-                    
-                    //transform.GetComponent<SplineFollower>().motion.rotationOffset += new Vector3(0, -rotY)*5*Time.fixedDeltaTime;
-                    transform.DORotate(new Vector3(0, -5, 0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(()=>Rot());
+                    transform.DORotate(new Vector3(0, -6, 0), 0.35f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(()=>Rot());
 
                 }
                 else if (touchXDelta <= 7f && touchXDelta > 0)
                 {
-                    transform.DORotate(new Vector3(0, 23, 0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot1());
+                    transform.DORotate(new Vector3(0, 24, 0), 0.35f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot1());
 
                 }
 
@@ -104,24 +102,21 @@ public class PlayerController : MonoBehaviour
                 
                 touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
                 offset = transform.GetComponent<SplineFollower>().motion.offset.x + 25f * touchXDelta * Time.fixedDeltaTime;
-                offset = Mathf.Clamp(offset, -7f, 7f);
+                offset = Mathf.Clamp(offset, -6f, 7f);
+                calendarOffset = transform.GetComponent<SplineFollower>().motion.offset.x + 30f * touchXDelta * Time.fixedDeltaTime;
+                calendarOffset = Mathf.Clamp(calendarOffset, -6f, 7f);
                 transform.GetComponent<SplineFollower>().motion.offset = new Vector3(offset, yOffset );
-                calendar.transform.GetComponent<SplineFollower>().motion.offset = new Vector3(offset, yOffset);
-                /*rotOffset = transform.GetComponent<SplineFollower>().motion.rotationOffset.y + 25f* touchXDelta*Time.fixedDeltaTime;
-                rotOffset = Mathf.Clamp(rotOffset, -6f, 24f);
-                transform.GetComponent<SplineFollower>().motion.rotationOffset = new Vector3(0, rotOffset);*/
+                calendar.transform.GetComponent<SplineFollower>().motion.offset = new Vector3(calendarOffset, 0.7f);
+               
 
-                if (touchXDelta >= -7f && touchXDelta < 0)
+                if (touchXDelta >= -6f && touchXDelta < 0)
                 {
-                    transform.DORotate(new Vector3(0, -6, 0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot1());
-
-
-
+                    transform.DORotate(new Vector3(0, -6, 0), 0.35f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot1());
 
                 }
                 else if (touchXDelta <= 7f && touchXDelta > 0)
                 {
-                    transform.DORotate(new Vector3(0, 24, 0), 0.2f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot());
+                    transform.DORotate(new Vector3(0, 24, 0), 0.35f, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(() => Rot());
                 }
 
             }
@@ -143,7 +138,6 @@ public class PlayerController : MonoBehaviour
         {
             transform.DOScale(1.1f, 0.3f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
             IncreaseScore(-5);
-            //transform.DOShakePosition(1f, new Vector3(1, 0, 0), 50, 1, false, true);
         }
        
 
@@ -157,7 +151,9 @@ public class PlayerController : MonoBehaviour
             GetComponent<SplineFollower>().motion.rotationOffset = new Vector3(0, 9);
             LevelController.Current.calendar.GetComponent<SplineFollower>().follow = false;
             calendar.SetActive(false);
-            LevelController.Current.gameActive = false;    
+            LevelController.Current.gameActive = false;
+            RenderSettings.skybox = skyboxMat;
+            Camera.main.clearFlags = CameraClearFlags.Skybox;
 
 
         }
@@ -172,6 +168,7 @@ public class PlayerController : MonoBehaviour
             LevelController.Current.finishGameMenu.SetActive(true);
             LevelController.Current.levelStartMenu.SetActive(false);
             follower.follow = false;
+           
 
         }
 
@@ -191,15 +188,19 @@ public class PlayerController : MonoBehaviour
 
                 tudorHouse.SetActive(false);
                 dutchHouse.SetActive(true);
-                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1400s";
+                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1625s";
                 calendar.transform.GetChild(0).GetChild(0).GetChild(0).transform.DOScale(1.2f, 0.2f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+                
+                //popup.rectTransform.DOScale(1, 1f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+                
+               
             }
 
             if (score >= 20)
             {
                 frenchHouse.SetActive(true);
                 dutchHouse.SetActive(false);
-                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1500s";
+                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1650s";
                 calendar.transform.GetChild(0).GetChild(0).GetChild(0).transform.DOScale(1.2f, 0.2f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
             }
 
@@ -209,7 +210,7 @@ public class PlayerController : MonoBehaviour
                 spanishHouse.SetActive(true);
                 englishHouse.SetActive(false);
                 dutchHouse.SetActive(false);
-                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1600s";
+                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1675s";
                 calendar.transform.GetChild(0).GetChild(0).GetChild(0).transform.DOScale(1.2f, 0.2f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
             }
             
@@ -225,7 +226,7 @@ public class PlayerController : MonoBehaviour
             {
                 dutchHouse.SetActive(false);
                 tudorHouse.SetActive(true);
-                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1300s";
+                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1600s";
                 calendar.transform.GetChild(0).GetChild(0).GetChild(0).transform.DOScale(1.2f, 0.2f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
             }
 
@@ -233,7 +234,7 @@ public class PlayerController : MonoBehaviour
             {
                 frenchHouse.SetActive(false);
                 dutchHouse.SetActive(true);
-                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1400s";
+                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1625s";
                 calendar.transform.GetChild(0).GetChild(0).GetChild(0).transform.DOScale(1.2f, 0.2f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
             }
 
@@ -241,7 +242,7 @@ public class PlayerController : MonoBehaviour
             {
                 frenchHouse.SetActive(true);
                 spanishHouse.SetActive(false);
-                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1500s";
+                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1650s";
                 calendar.transform.GetChild(0).GetChild(0).GetChild(0).transform.DOScale(1.2f, 0.2f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
             }
 
@@ -249,7 +250,7 @@ public class PlayerController : MonoBehaviour
             {
                 spanishHouse.SetActive(true);
                 englishHouse.SetActive(false);
-                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1600s";
+                calendar.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = "1675s";
                 calendar.transform.GetChild(0).GetChild(0).GetChild(0).transform.DOScale(1.2f, 0.2f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
             }
         }
@@ -263,6 +264,7 @@ public class PlayerController : MonoBehaviour
         score += increment;
     }
 
+  
     
 
     void Rot()
